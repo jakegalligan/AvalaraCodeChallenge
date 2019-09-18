7a19ad1caddd1cd734e5126a7c51502855dd65b6
jakegalligan <galligan1996@gmail.com>
2019-09-18 19:11:51 -0400

    When the user clicks submit on the date input form that data is stored within a variable

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace AvalaraCodeChallenge.Pages
 {
@@ -13,7 +16,27 @@ namespace AvalaraCodeChallenge.Pages
         {
         //when the date form is submitted store the input data into a variable
         var date = Request.Form["date"];
-        
+        //  string connectionString = "server=localhost;user=root;database=weather;port=3306;password=jg1996";
+        // MySqlConnection connection = new MySqlConnection(connectionString);
+        // try 
+        // {
+        //     System.Diagnostics.Debug.WriteLine("Connected");
+        //     connection.Open();
+        //     string query = "SELECT * FROM precipdata";
+        //     MySqlCommand command = new MySqlCommand(query, connection);
+        //     MySqlDataReader reader = command.ExecuteReader();
+
+        //     while (reader.Read()) {
+        //         Console.WriteLine(reader[1]);
+        //     }
+
+
+        // }
+        //  catch (Exception ex)
+        // {
+        //     Console.WriteLine(ex.ToString());
+        // }
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ namespace AvalaraCodeChallenge
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-    }
+    } 
 }
+
